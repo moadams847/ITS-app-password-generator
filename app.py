290ab416171269abc8c5bec4aec87d92eb19d7cc
Adams Mohammed <moadams847@gmail.com>
2021-02-17 10:18:55 +0000

    major changes to app.py

--- app.py
+++ app.py
@@ -14,26 +14,35 @@ def password():
 
     if request.method == 'POST':
 
-        passlen = int(request.form['password_length'])
-        if passlen <= 12:
-            # print(passlen)
-            s="abcdefghjkmnpqrstuvwxyz23456789!#$%&-=+?ABCDEFGHJKMNPQRSTUVWXYZ!#$%&-=+?"
-            p = "".join(random.sample(s,passlen ))
-            return render_template('home.html', gen_pas = f'Temporary ITS password generated:     {p}')
+        try:
+
+            passlen = int(request.form['password_length'])
+            s= "!#$%&-=+?abcdefghjk!#$%&-=+?mnpqrstuvwxyz23456789!#$%&-=+?ABCDEFGHJKMNPQRSTUVWXYZ!#$%&-=+?"
+            str_ = "abcdefghjkmnpqrstuvwxyz23456789ABCDEFGHJKMNPQRSTUVWXYZ"
+            
+
+            if passlen > 0  and passlen <= 12:
+                
+                p = "".join(random.sample(s,passlen ))
+                return render_template('home.html', gen_pas = f'Temporary ITS password generated:     {p}')
+
+            elif passlen <= 0:
+                return render_template('home.html', gen_pas = f'Password length must be greater 0')
+
+            elif passlen > 12 and  passlen <= 54:
+
+                p = "".join(random.sample(str_,passlen ))
+                return render_template('home.html', gen_pas = f'Temporary ITS password generated:     {p}')
+            
+            elif passlen > 54:
+                return render_template('home.html', gen_pas = f'Password length must be less than 55')
 
 
-        else:
-
-            s="abcdefghjkmnpqrstuvwxyz23456789ABCDEFGHJKMNPQRSTUVWXYZ"
-            p = "".join(random.sample(s,passlen ))
-            return render_template('home.html', gen_pas = f'Temporary ITS password generated:     {p}')
-
-
-        # print(p)
-
-
-    return render_template('home.html')
-
-
+        except:
+            return render_template('home.html', gen_pas = f'Wrong entry, Provide a number !!')
+                    
+    else: 
+        return render_template('home.html')
+               
 if __name__ == '__main__':
     app.run(debug = True)
