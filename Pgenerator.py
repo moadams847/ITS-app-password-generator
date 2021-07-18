91b9710bc85546bda4e362f56c6c7bab90f7f21f
Adams Mohammed <moadams847@gmail.com>
2021-07-18 10:33:07 +0000

    update ui, create Pgenerate function

---
+++ Pgenerator.py
@@ -0,0 +1,12 @@
+import random
+
+def generate_password(passlen):
+    s= "abcdefghjk!$%&+?mnpqrstuvwxyz23456789!$%&+?ABCDEFGHJKMNPQRSTUVWXYZ!$%&+?"
+    p = "".join(random.sample(s,passlen ))
+    return p
+
+
+    
+if __name__ == '__main__':
+    user = input('Enter number: ')
+    print(generate_password(int(user)))
--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, render_template
-import random
+from Pgenerator import generate_password
 
 app = Flask(__name__)
 
@@ -10,38 +10,30 @@ def home():
 
 @app.route("/password", methods = ['POST', 'GET'])
 def password():
-
-    if request.method == 'POST':
-
-        try:
-
+    try:
+            
+        if request.method == 'POST':
             passlen = int(request.form['password_length'])
-            s= "abcdefghjk!$%&+?mnpqrstuvwxyz23456789!$%&+?ABCDEFGHJKMNPQRSTUVWXYZ!$%&+?"
-            str_ = "abcdefghjkmnpqrstuvwxyz23456789ABCDEFGHJKMNPQRSTUVWXYZ"
             
+        if passlen > 0  and passlen <= 12:
+            p = generate_password(passlen)
+            return render_template('home.html', gen_pas = f'{p}')
 
-            if passlen > 0  and passlen <= 12:
-                
-                p = "".join(random.sample(s,passlen ))
-                return render_template('home.html', gen_pas = f'{p}')
+        elif passlen <= 0:
+            return render_template('home.html', gen_pas = f'Password length must be greater 0')
 
-            elif passlen <= 0:
-                return render_template('home.html', gen_pas = f'Password length must be greater 0')
-
-            elif passlen > 12 and  passlen <= 54:
-
-                p = "".join(random.sample(str_,passlen ))
-                return render_template('home.html', gen_pas = f'{p}')
+        elif passlen > 12 and  passlen <= 54:
+            p = generate_password(passlen)
+            return render_template('home.html', gen_pas = f'{p}')
             
-            elif passlen > 54:
-                return render_template('home.html', gen_pas = f'Password length must be less than 55')
-
-
-        except:
-            return render_template('home.html', gen_pas = f'Wrong entry, Provide a number!!')
-                    
-    else: 
+        elif passlen > 54:
+            return render_template('home.html', gen_pas = f'Password length must be less than 55')
+            
+    except:
         return render_template('home.html')
+
+            
+        
                
 if __name__ == '__main__':
     app.run(debug = True)
