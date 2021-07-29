4a0ad8dc6db281224ae6aeac1f627d9dd87aa79b
Adams Mohammed <moadams847@gmail.com>
2021-07-29 00:56:13 +0000

    update app.py home.html and create password.html

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route("/password", methods = ['POST'])
+@app.route("/password", methods = ['POST', 'GET'])
 def password():
     try:
             
@@ -16,17 +16,17 @@ def password():
             
         if passlen > 0  and passlen <= 12:
             p = generate_password(passlen)
-            return render_template('home.html', gen_pas = f'{p}')
+            return render_template('password.html', gen_pas = f'{p}')
 
         elif passlen <= 0:
-            return render_template('home.html', gen_pas = f'Password length must be greater 0')
+            return render_template('password.html', gen_pas = f'Password length must be greater 0')
 
         elif passlen > 12 and  passlen <= 54:
             p = generate_password(passlen)
-            return render_template('home.html', gen_pas = f'{p}')
+            return render_template('password.html', gen_pas = f'{p}')
             
         elif passlen > 54:
-            return render_template('home.html', gen_pas = f'Password length must be less than 55')
+            return render_template('password.html', gen_pas = f'Password length must be less than 55')
             
     except:
         return render_template('home.html')
