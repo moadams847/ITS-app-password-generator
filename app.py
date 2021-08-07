e3eec1a8454206d0d7cb7b77fd25a839e2942574
Adams Mohammed <moadams847@gmail.com>
2021-08-07 03:28:40 +0000

    add flask cors

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, request, jsonify
 from Pgenerator import generate_password
 
+# https://stackoverflow.com/q/20035101
+# flask cors
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def home():
